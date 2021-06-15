3506ebce9c9136cafbb8fed6295726337829a4cb
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-06-15 15:10:17 +0300

    add he

--- route/dist/enRoute.dev.js
+++ route/dist/enRoute.dev.js
@@ -8,6 +8,6 @@ var router = Router();
 var path = require('path');
 
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'client', 'en', 'index.html')); // console.log(path.join(__dirname, '..', 'client', 'en', 'index.html'))
+  res.sendFile(path.join(__dirname, '..', 'client', 'en', 'index.html'));
 });
 module.exports = router;
--- route/enRoute.js
+++ route/enRoute.js
@@ -4,7 +4,6 @@ const path = require('path')
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'en', 'index.html'))
-    // console.log(path.join(__dirname, '..', 'client', 'en', 'index.html'))
 })
 
 module.exports = router;
